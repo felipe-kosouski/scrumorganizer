2528e5518489d55777cf80910813abf4ef9eb702
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-03-28 18:13:46 -0300

    Configuration, home page, register and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Root Route
+  root to: 'home#index'
+
+  # Guest Routes
+  get 'home/register'
+  get 'home/login'
+
 end
