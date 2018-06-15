d90f3b0a80c596125cefe97ac7e8df07b5525996
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-06-15 16:42:45 -0300

    Added image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,26 @@ class User < ApplicationRecord
   VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]*[_|-|.]*[a-zA-Z0-9]*\z/
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+  mount_uploader :picture, PictureUploader
+
   validates :name, presence: true
   validates :username, presence: true,
             format: {with: VALID_USERNAME_REGEX,
                      message: "Somente letras, numeros e simbolos (. _ -)"}
   validates :email, presence: true,
             format: {with: VALID_EMAIL_REGEX}
+  validate :picture_size
 
   has_many :projects
   has_many :users_projects
   has_many :shared_projects, through: :users_projects, source: :project
+
+  private
+
+  # Validates the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "A imagem deve ser menor que 5MB")
+    end
+  end
 end
