cc74a34190fe57dbcdec16265f237f07d23a009d
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-06-22 15:10:04 -0300

    Added roles radio buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   rolify
 
+  ROLES = %w[manager master developer]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
